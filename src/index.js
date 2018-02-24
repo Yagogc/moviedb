b7605767f3dc6028d8542c46f8ebad59939972c0
Yago <jagogc@gmail.com>
2018-02-24 21:31:43 +0000

    Moving to styled-components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {injectGlobal} from 'styled-components'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+injectGlobal`
+* {
+	margin: 0;
+	padding: 0;
+	font-family: sans-serif;
+	box-sizing: border-box;
+}
+`
