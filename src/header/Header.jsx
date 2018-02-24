607448ee24319e52df7be179a22f56ea08e984f8
Yago <jagogc@gmail.com>
2018-02-24 21:44:59 +0000

    Style changes to Header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -11,8 +11,14 @@ export default Header;
 
 const Head = styled.header`
 	text-align: center;
-
+	display: flex;
+	height: 80px;
+	justify-content: center;
+	align-items: center;
+	border-bottom: 1px solid rebeccapurple;
 	h1 {
+		font-size: 2rem;
+		color: rebeccapurple;
 		text-transform: uppercase;
 	}
 	span {
