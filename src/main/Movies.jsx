f14ffde80d2a5cd459ef02b1b042233db6753a57
Yago <jagogc@gmail.com>
2018-02-24 22:04:48 +0000

    Fetching movies from TMDB

--- src/main/Movies.jsx
+++ src/main/Movies.jsx
@@ -1,11 +1,61 @@
 
 import React from "react";
+import MovieListItem from "./MovieListItem";
 
+
+const tmdb = 'c857fa67fba523ad3ce66df18e7ab279';
+const movies = ["Breaking bad", "Narcos", "Game of Thrones"];
 class Movies extends React.Component {
+	
+	state = {
+		movies: []
+	}
+	
+	storeMovies = data => {
+		const movies = data.results.map( result => {
+			const {
+				vote_count,
+				id,
+				genre_ids,
+				poster_path,
+				title,
+				vote_average,
+				release_date
+			} = result;
+			return {
+				vote_count,
+				id,
+				genre_ids,
+				poster_path,
+				title,
+				vote_average,
+				release_date
+			};
+		});
+	
+		this.setState({ movies })
+	}
+	componentDidMount() {
+		const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+	
+		fetch(apiUrl)
+			.then(response => response.json())
+			.then(data => this.storeMovies(data))
+			.catch(error => console.log(error))
+	
+		console.log("Before or after data?");
+	}
+	
 	render() {
 		return (
 		<section>
-			Movies
+			<ul className="movies">
+			{
+			this.state.movies.map( movie => (
+				<MovieListItem key={movie.id} movie={movie} />
+				))
+			}
+			</ul>
 		</section>
 		)
 	}
